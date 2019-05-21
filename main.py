7c40744e499d49bbeb26f673a4ff89cbf1964139
zapathy <apathy.zsombor@gmail.com>
2019-05-21 09:42:13 +0200

    fixed warnings

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from urllib.request import urlopen, Request
-import bs4
+import json
 import re
 import sys
+from urllib.request import urlopen, Request
+
+import bs4
 import requests
-import datetime
 
 
 def main():
+    global total_pages
     url = 'https://www.towerbudapest.com/en/sales'
     hdr = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 '
+                      'Safari/537.11',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
         'Accept-Encoding': 'none',
@@ -35,7 +38,6 @@ def main():
         rightButton = soup.select('div[class*="text-right"]')[0].select('button')
         print('.', end='')
         sys.stdout.flush()
-
         if len(rightButton) != 0:
             url = rightButton[0]['onclick'].split("'")[1]
         else:
@@ -51,26 +53,18 @@ def main():
     print()
     counter = 0
     status_codes = {}
+    exceptions = []
     for link in property_links:
         try:
             counter += 1
             if counter % 10 == 0:
                 print('.', end='')
                 sys.stdout.flush()
-            # if counter > 100:
-            #     break
             property_data = {}
             content = urlopen(Request(link, headers=hdr)).read()
             soup = bs4.BeautifulSoup(content, features="lxml")
             header = soup.select('div[class*="property-content"]')[0].select('h1')[0].contents[0]
             property_data['name'] = header
-            # if len(soup.select('div[class*="property-details-content-description"]')[0].select('p')) > 1:
-            #     description = soup.select('div[class*="property-details-content-description"]')[0].select('p')[1].contents[
-            #         0]
-            # else:
-            #     description = soup.select('div[class*="property-details-content-description"]')[0].select('p')[0].contents[
-            #         0]
-            # # property_data['description'] = description
             details = soup.select('div[class*="property-details-sidebar"]')[0]
 
             for listitem in details.select('ul')[0].select('li'):
@@ -87,16 +81,13 @@ def main():
 
             property_data['pricehuf'] = details.select('ul')[1].select('li')[1].contents[0].split()[0].replace('.', '')
             if len(details.select('ul')[1].select('li')) > 2:
-                property_data['priceeur'] = details.select('ul')[1].select('li')[2].contents[0].split()[0].replace('.', '')
+                property_data['priceeur'] = details.select('ul')[1].select('li')[2].contents[0].split()[0].replace('.',
+                                                                                                                   '')
 
             # property_data['contact'] = details.select('ul')[2].select('li')[1].contents[0]
 
             property_data['name'] = property_data['name'].lower()
             recognized_suffixes = ['utca', 'út', 'tér', 'park']
-            recognized_suffix_aliases = [
-                ['utca', 'street'],
-                ['út', 'road'],
-                ['tér', 'square'], 'park']
             recognized_suffixes_english = ['street', 'road', 'square', 'park']
             split_name = (str(property_data['name'])).split()
             for s in split_name:
@@ -113,16 +104,24 @@ def main():
 
             property_data_list.append(property_data)
             r = requests.post("https://propertybuddy-database.herokuapp.com/properties", json=property_data)
-            if r.status_code in status_codes:
-                status_codes[r.status_code] += 1
+
+            latest_status_code = (r.status_code, json.loads(r.content)['message'])
+            if latest_status_code in status_codes:
+                status_codes[latest_status_code] += 1
             else:
-                status_codes[r.status_code] = 1
-        except:
+                status_codes[latest_status_code] = 1
+        except Exception as e:
+            exceptions.append(e)
             pass
     print()
     print()
     print('done with status codes:')
     for c in status_codes:
-        print("\t" + str(c) + " x" + str(status_codes[c]))
+        print(str(c[0]) + " x" + "\t" + str(status_codes[c]) + "\n\t" + str(c[1]))
+    print()
+    print('and exceptions:')
+    for e in exceptions:
+        print(e)
+
 
 main()
